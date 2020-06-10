9628df5bfb0712b61f4e0ada7bbe190daa198fea
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-10 16:54:14 +0900

    add status code helpers

---
+++ app/server/handlers/helpers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bootcamp-tpa/course.summarist-service_go/logs"
+)
+
+const (
+	contentType = "Content-Type"
+	authToken   = "X-Auth-Token"
+	jsonContent = "application/json"
+	mp3Content  = "audio/mpeg"
+)
+
+func logRequest(r *http.Request) {
+	logs.Info("Received request: %s", requestSummary(r))
+}
+
+func requestSummary(r *http.Request) string {
+	return fmt.Sprintf("%v %v", r.Method, r.URL)
+}
+
+func BadRequest(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "400 bad request", http.StatusBadRequest)
+}
+
+func Unauthorized(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "401 unauthorized", http.StatusUnauthorized)
+}
+
+func Forbidden(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "403 forbidden", http.StatusForbidden)
+}
+
+func Conflict(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "409 conflict", http.StatusConflict)
+}
+
+func InternalServerError(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "500 internal server error", http.StatusInternalServerError)
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "404 Not Found", http.StatusNotFound)
+}
