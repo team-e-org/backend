9bf97eb84523bd50ded91dd0df7d8de4d8f9ceaa
coil398 <konayuta.sou@gmail.com>
2020-06-10 18:01:41 +0900

    add mocks

---
+++ app/mocks/tag.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"app/models/db"
+	"app/repository"
+)
+
+type TagMock struct {
+	ExpectedTag *db.Tag
+}
+
+func NewTagRepository() repository.TagRepository {
+	return &TagMock{}
+}
+
+func (m *TagMock) AddTag(tag *db.Tag) error {
+	m.ExpectedTag = tag
+	return nil
+}
+
+func (m *TagMock) GetTag(tagID int) (*db.Tag, error) {
+	return m.ExpectedTag, nil
+}
