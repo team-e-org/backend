30d4c7326a175114843440202e33a8774a2be732
Naoto Ishida <naoto.ishida@dena.com>
2020-06-12 16:26:39 +0900

    Add error case to authenticate user test

--- app/authz/authentication_test.go
+++ app/authz/authentication_test.go
@@ -8,24 +8,47 @@ import (
 )
 
 func TestAuthLayer_AuthenticateUser(t *testing.T) {
-	user := &models.User{
-		ID:             0,
-		Name:           "naoto",
-		HashedPassword: "$2a$10$SOWUFP.hkVI0CrCJyfh5vuf/Gu.SDpv6Y2DYZ/Dbwyr.AKtlAldFe",
+	tests := []struct {
+		password       string
+		hashedPassword string
+		wantError      bool
+	}{
+		{
+			"password",
+			"$2a$10$SOWUFP.hkVI0CrCJyfh5vuf/Gu.SDpv6Y2DYZ/Dbwyr.AKtlAldFe",
+			false,
+		},
+		{
+			"password",
+			"$2a$10$a75046RPLEzoN4ObqiOS7Oh/NcvWagI68GHvygZpj3DMvCAekU/1W",
+			true,
+		},
 	}
-	userRepo := mocks.NewUserRepository()
-	_ = userRepo.CreateUser(user)
-	storage := &db.DataStorage{
-		Users: userRepo,
-	}
-	al := NewAuthLayer(*storage)
 
-	token, err := al.AuthenticateUser("naoto", "password")
-	if err != nil {
-		t.Fatalf("An error occured: %v", err)
-	}
+	for _, tt := range tests {
+		user := &models.User{
+			ID:             0,
+			Name:           "user",
+			HashedPassword: tt.hashedPassword,
+		}
+		userRepo := mocks.NewUserRepository()
+		_ = userRepo.CreateUser(user)
+		storage := &db.DataStorage{
+			Users: userRepo,
+		}
+		al := NewAuthLayer(*storage)
 
-	if len(token) != 36 {
-		t.Fatalf("invalid token: got %s", token)
+		token, err := al.AuthenticateUser("user", tt.password)
+		if !tt.wantError && err != nil {
+			t.Fatalf("An error occured: %v", err)
+		}
+
+		if tt.wantError && err == nil {
+			t.Fatalf("Error should occur")
+		}
+
+		if !tt.wantError && len(token) != 36 {
+			t.Fatalf("invalid token length: got %s", token)
+		}
 	}
 }
