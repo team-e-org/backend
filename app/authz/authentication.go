9a8ead958816bb2c5d65971b1c8c964bfdaf1766
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-12 11:27:08 +0900

    user auth function

---
+++ app/authz/authentication.go
@@ -0,0 +1,66 @@
+package authz
+
+import (
+	"app/authz/token"
+	"app/authz/token/storage"
+	"app/db"
+	"app/models"
+	"encoding/json"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrInvalidPassword = errors.New("password is not collect")
+var ErrInvalidToken = errors.New("token is invalid")
+
+type TokenData struct {
+	UserData *models.User
+}
+
+type AuthLayerInterface interface {
+	AuthenticateUser(string, string) (string, error)
+}
+
+type AuthLayer struct {
+	tokenStorage storage.TokenStorage
+	dataStorage  db.DataStorage
+}
+
+func NewAuthLayer(data db.DataStorage) AuthLayerInterface {
+	tokenStorage := storage.NewInMemoryTokenStorage()
+	return &AuthLayer{
+		tokenStorage,
+		data,
+	}
+}
+
+func (a *AuthLayer) AuthenticateUser(email string, password string) (string, error) {
+	user, err := a.dataStorage.Users.GetUserByEmail(email)
+	if err != nil {
+		return "", err
+	}
+
+	passwordCheckError := checkUserPassword(password, user.HashedPassword)
+	if passwordCheckError != nil {
+		return "", ErrInvalidPassword
+	}
+
+	bytes, err := json.Marshal(&TokenData{
+		UserData: user,
+	})
+	if err != nil {
+		return "", ErrInvalidToken
+	}
+
+	token := token.NewToken()
+	if err = a.tokenStorage.SetTokenData(token, string(bytes)); err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func checkUserPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
