19192c553210a68538fc873e17fd0035932f1cd6
coil398 <konayuta.sou@gmail.com>
2020-06-12 11:45:41 +0900

    add func to create db config

--- app/config/config.go
+++ app/config/config.go
@@ -23,27 +23,38 @@ type DBConfig struct {
 	Port     int
 }
 
+func ReadDBConfig() (*DBConfig, error) {
+	dbPort, err := strconv.Atoi(os.Getenv("MYSQL_PORT"))
+	if err != nil {
+		return nil, fmt.Errorf("reading env var 'MYSQL_PORT': %w", err)
+	}
+
+	dbConfig := &DBConfig{
+		User:     os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		DBName:   os.Getenv("MYSQL_DATABASE"),
+		Host:     os.Getenv("MYSQL_HOST"),
+		Port:     dbPort,
+	}
+
+	return dbConfig, nil
+}
+
 func ReadConfig() (*Config, error) {
 	port, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
 		return nil, fmt.Errorf("reading env var 'SERVER_PORT': %w", err)
 	}
 
-	dbPort, err := strconv.Atoi(os.Getenv("MYSQL_PORT"))
+	dbConfig, err := ReadDBConfig()
 	if err != nil {
-		return nil, fmt.Errorf("reading env var 'MYSQL_PORT': %w", err)
+		return nil, err
 	}
 
 	return &Config{
 		Server{
 			Port: port,
 		},
-		DBConfig{
-			User:     os.Getenv("MYSQL_USER"),
-			Password: os.Getenv("MYSQL_PASSWORD"),
-			DBName:   os.Getenv("MYSQL_DATABASE"),
-			Host:     os.Getenv("MYSQL_HOST"),
-			Port:     dbPort,
-		},
+		*dbConfig,
 	}, nil
 }
