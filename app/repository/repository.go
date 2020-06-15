d61894e7017e303354b9b7477b75c557d3be89db
coil398 <konayuta.sou@gmail.com>
2020-06-15 10:38:36 +0900

    add methods to repository interfaces

--- app/repository/repository.go
+++ app/repository/repository.go
@@ -4,21 +4,30 @@ import "app/models"
 
 type UserRepository interface {
 	CreateUser(user *models.User) error
+	UpdateUser(user *models.User) error
+	DeleteUser(userID int) error
 	GetUser(userID int) (*models.User, error)
 	GetUserByEmail(email string) (*models.User, error)
 }
 
 type BoardRepository interface {
 	CreateBoard(board *models.Board) error
+	UpdateBoard(board *models.Board) error
+	DeleteBoard(boardID int) error
 	GetBoard(boardID int) (*models.Board, error)
 }
 
 type PinRepository interface {
 	CreatePin(pin *models.Pin) error
+	UpdatePin(pin *models.Pin) error
+	DeletePin(pinID int) error
 	GetPin(pinID int) (*models.Pin, error)
+	GetPinsByBoardID(boardID int) ([]*models.Pin, error)
+	GetPinsByUserID(userID int) ([]*models.Pin, error)
 }
 
 type TagRepository interface {
 	CreateTag(tag *models.Tag) error
 	GetTag(tagID int) (*models.Tag, error)
+	GetTagByPinID(pinID int) ([]*models.Tag, error)
 }
