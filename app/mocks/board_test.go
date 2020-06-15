07bb9c2ace063d7f7eaf11301b5527279f6614df
coil398 <konayuta.sou@gmail.com>
2020-06-15 15:58:02 +0900

    mock board test passed

--- app/mocks/board_test.go
+++ app/mocks/board_test.go
@@ -50,3 +50,54 @@ func TestBoardMockRepository(t *testing.T) {
 		t.Fatalf("Not equal board")
 	}
 }
+
+func TestBoard(t *testing.T) {
+	boards := NewBoardRepository()
+	ID := 0
+	UserID := 0
+	now := time.Now()
+	board := &models.Board{
+		ID:          ID,
+		UserID:      UserID,
+		Name:        "test board",
+		Description: "test description",
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+	boards.CreateBoard(board)
+	board2 := &models.Board{
+		ID:          ID,
+		UserID:      UserID,
+		Name:        "test2 board",
+		Description: "test2 description",
+		CreatedAt:   now,
+		UpdatedAt:   time.Now(),
+	}
+	boards.UpdateBoard(board2)
+	b, err := boards.GetBoard(ID)
+	if err != nil {
+		t.Fatalf("An error occurred: %v\n", err)
+	}
+	if testCompareBoards(t, b, board) {
+		t.Fatalf("The board did not update")
+	}
+	if !testCompareBoards(t, b, board2) {
+
+	}
+}
+
+func testCompareBoards(t *testing.T, board *models.Board, board2 *models.Board) bool {
+	if board.ID != board2.ID {
+		return false
+	}
+	if board.UserID != board2.UserID {
+		return false
+	}
+	if board.Name != board2.Name {
+		return false
+	}
+	if board.Description != board2.Description {
+		return false
+	}
+	return true
+}
