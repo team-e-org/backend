72555fb6d608e3abfeecb968dcd61b964543516b
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-16 11:50:47 +0900

    get board by id

--- app/infrastructure/board.go
+++ app/infrastructure/board.go
@@ -28,6 +28,29 @@ func (u *Board) DeleteBoard(boardID int) error {
 	return nil
 }
 
-func (u *Board) GetBoard(boardID int) (*models.Board, error) {
-	return nil, nil
+func (b *Board) GetBoard(boardID int) (*models.Board, error) {
+	const query = `
+SELECT b.id, b.user_id, b.name, b.description, b.is_private, b.is_archive, b.created_at, b.updated_at
+FROM boards b
+WHERE b.id = ?;
+`
+	row := b.DB.QueryRow(query, boardID)
+
+	board := &models.Board{}
+	err := row.Scan(
+		&board.ID,
+		&board.UserID,
+		&board.Name,
+		&board.Description,
+		&board.IsPrivate,
+		&board.IsArchive,
+		&board.CreatedAt,
+		&board.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return board, nil
 }
