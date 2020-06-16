76e11210780474d9b8231b8bbd23a3a17f81ae93
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-16 19:11:15 +0900

    test view.NewPins

--- app/view/pin_test.go
+++ app/view/pin_test.go
@@ -48,3 +48,61 @@ func TestPin(t *testing.T) {
 		t.Fatalf("IsPrivate does not match, model: %v, view: %v", p.IsPrivate, v.IsPrivate)
 	}
 }
+
+func TestNewPins(t *testing.T) {
+	pins := []*models.Pin{{
+		ID:          0,
+		UserID:      0,
+		Title:       "test title 1",
+		Description: "test description 1",
+		URL:         "test url 1",
+		ImageURL:    "test image url 1",
+		IsPrivate:   false,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}, {
+		ID:          0,
+		UserID:      0,
+		Title:       "test title 2",
+		Description: "test description 2",
+		URL:         "test url 2",
+		ImageURL:    "test image url 2",
+		IsPrivate:   false,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}}
+
+	newPins := NewPins(pins)
+
+	for i, newPin := range newPins {
+		p := pins[i]
+
+		if p.ID != newPin.ID {
+			t.Fatalf("ID does not match, model: %v, view: %v", p.ID, newPin.ID)
+		}
+
+		if p.UserID != newPin.UserID {
+			t.Fatalf("UserID does not match, model: %v, view: %v", p.UserID, newPin.UserID)
+		}
+
+		if p.Title != newPin.Title {
+			t.Fatalf("Title does not match, model: %v, view: %v", p.Title, newPin.Title)
+		}
+
+		if p.Description != newPin.Description {
+			t.Fatalf("Description does not match, model: %v, view: %v", p.Description, newPin.Description)
+		}
+
+		if p.URL != newPin.URL {
+			t.Fatalf("URL does not match, model: %v, view: %v", p.URL, newPin.URL)
+		}
+
+		if p.ImageURL != newPin.ImageURL {
+			t.Fatalf("ImageURL does not match, model: %v, view: %v", p.ImageURL, newPin.ImageURL)
+		}
+
+		if p.IsPrivate != newPin.IsPrivate {
+			t.Fatalf("IsPrivate does not match, model: %v, view: %v", p.IsPrivate, newPin.IsPrivate)
+		}
+	}
+}
