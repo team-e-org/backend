6e709a7bac0a238110b6319afd7138470b3f57a1
coil398 <konayuta.sou@gmail.com>
2020-06-18 14:35:25 +0900

    add sql get user test

--- app/infrastructure/user_test.go
+++ app/infrastructure/user_test.go
@@ -32,7 +32,7 @@ func TestMain(m *testing.M) {
 	os.Exit(mockDBHandlingWrapper(m))
 }
 
-func TestUserRepositorySQLMock(t *testing.T) {
+func TestInsertUser(t *testing.T) {
 	now := time.Now()
 	user := &models.User{
 		ID:             0,
@@ -51,6 +51,20 @@ func TestUserRepositorySQLMock(t *testing.T) {
 
 	users := NewUserRepository(sqlDB)
 	err := users.CreateUser(user)
+	if err != nil {
+		t.Fatalf("An error occurred: %v\n", err)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	now := time.Now()
+	prepare := mock.ExpectPrepare("SELECT u.id, u.Name, u.email, u.password, u.icon. u.created_at, u.updated_at FROM users u WHERE u.id = ?")
+	prepare.ExpectQuery().WithArgs(0).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "password", "icon", "created_at", "updated_at"}).
+			AddRow(0, "test user", "test@test.com", "password", "test icon", now, now))
+
+	users := NewUserRepository(sqlDB)
+	_, err := users.GetUser(0)
 	if err != nil {
 		t.Fatalf("An error occurred: %v\n", err)
 	}
