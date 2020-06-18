dc6b8f3dad53d18946ea36e7f43a7e5dfc57115a
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-18 18:06:24 +0900

    get tokendata test

--- app/authz/authentication_test.go
+++ app/authz/authentication_test.go
@@ -4,6 +4,7 @@ import (
 	"app/db"
 	"app/mocks"
 	"app/models"
+	"reflect"
 	"testing"
 )
 
@@ -65,4 +66,38 @@ func TestAuthLayer_AuthenticateUser(t *testing.T) {
 			t.Fatalf("invalid token length: got %s", token)
 		}
 	}
+}
+
+func TestAuthLayer_GetTokenData(t *testing.T) {
+	test := struct {
+		registeredEmail string
+		hashedPassword  string
+		email           string
+		password        string
+	}{
+		"abc@example.com",
+		"$2a$10$SOWUFP.hkVI0CrCJyfh5vuf/Gu.SDpv6Y2DYZ/Dbwyr.AKtlAldFe",
+		"abc@example.com",
+		"password",
+	}
+
+	user := &models.User{
+		ID:             0,
+		Email:          test.registeredEmail,
+		HashedPassword: test.hashedPassword,
+	}
+	userRepo := mocks.NewUserRepository()
+	_ = userRepo.CreateUser(user)
+	storage := &db.DataStorage{
+		Users: userRepo,
+	}
+	al := NewAuthLayer(*storage)
+
+	token, _ := al.AuthenticateUser(test.email, test.password)
+	tokenData, _ := al.GetTokenData(token)
+
+	if !reflect.DeepEqual(tokenData.UserData, user) {
+		t.Error("different token data")
+	}
+
 }
