db836605dad887881c46be851b728aa1540c0255
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-19 10:35:01 +0900

    add user_id when auth response

--- app/server/handlers/auth.go
+++ app/server/handlers/auth.go
@@ -29,7 +29,7 @@ func SignIn(data db.DataStorage, authLayer authz.AuthLayerInterface) func(http.R
 		}
 		userID, err := getUserIdByToken(authLayer, token)
 
-		response := view.NewLSignInResponse(token)
+		response := view.NewLSignInResponse(token, userID)
 		bytes, err := json.Marshal(response)
 		if err != nil {
 			logs.Error("Request: %s, serializing login response: %v", requestSummary(r), err)
@@ -82,7 +82,7 @@ func SignUp(data db.DataStorage, authLayer authz.AuthLayerInterface) func(http.R
 		}
 		userID, err := getUserIdByToken(authLayer, token)
 
-		response := view.NewLSignUpResponse(token)
+		response := view.NewLSignUpResponse(token, userID)
 		bytes, err := json.Marshal(response)
 		if err != nil {
 			logs.Error("Request: %s, serializing sign up response: %v", requestSummary(r), err)
