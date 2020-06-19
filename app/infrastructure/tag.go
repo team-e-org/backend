4942f63970db5d5db3c7245b962692628746993c
coil398 <konayuta.sou@gmail.com>
2020-06-19 11:04:14 +0900

    add sql create tag

--- app/infrastructure/tag.go
+++ app/infrastructure/tag.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"app/helpers"
 	"app/models"
 	"app/repository"
 	"database/sql"
@@ -16,18 +17,56 @@ func NewTagRepository(db *sql.DB) repository.TagRepository {
 	}
 }
 
-func (u *Tag) CreateTag(tag *models.Tag) error {
+func (t *Tag) CreateTag(tag *models.Tag) error {
+	const query = `
+INSERT INTO tags (tag) VALUES (?);
+`
+
+	stmt, err := t.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(tag.Tag)
+	err = helpers.CheckDBExecError(result, err)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (u *Tag) GetTag(tagID int) (*models.Tag, error) {
-	return nil, nil
+func (t *Tag) GetTag(tagID int) (*models.Tag, error) {
+	const query = `
+SELECT t.id, t.tag, t.created_at, t.updated_at FROM tags t WHERE t.id = ?;
+`
+
+	stmt, err := t.DB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	row := stmt.QueryRow(tagID)
+
+	tag := &models.Tag{}
+	err = row.Scan(
+		&tag.ID,
+		&tag.Tag,
+		&tag.CreatedAt,
+		&tag.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tag, nil
 }
 
-func (u *Tag) AttachTagToPin(tagID int, pinID int) error {
+func (t *Tag) AttachTagToPin(tagID int, pinID int) error {
 	return nil
 }
 
-func (u *Tag) GetTagsByPinID(pinID int) ([]*models.Tag, error) {
+func (t *Tag) GetTagsByPinID(pinID int) ([]*models.Tag, error) {
 	return nil, nil
 }
