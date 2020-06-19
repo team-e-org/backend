bad0c6b8a9d01e6356bae588645691ff72b2cf3d
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-19 21:52:59 +0900

    add init data

--- db/mysql/migrations/0000010_init_pins.sql
+++ db/mysql/migrations/0000010_init_pins.sql
@@ -84,4 +84,18 @@ VALUES
     "pin12",
     "pin12 description",
     "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+  ),
+  (
+    13,
+    1,
+    "pin13",
+    "pin13 description",
+    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+  ),
+  (
+    14,
+    1,
+    "pin14",
+    "pin14 description",
+    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
   );
