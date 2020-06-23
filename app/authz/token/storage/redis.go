6df9ff1f7f4e9dfc5915fec264e171156decbde7
coil398 <konayuta.sou@gmail.com>
2020-06-23 18:34:58 +0900

    add redis unit test

--- app/authz/token/storage/redis.go
+++ app/authz/token/storage/redis.go
@@ -1,6 +1,9 @@
 package storage
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis"
+)
 
 type redisTokenStorage struct {
 	redis *redis.Client
@@ -8,6 +11,20 @@ type redisTokenStorage struct {
 
 func NewRedisTokenStorage(redis *redis.Client) TokenStorage {
 	return &redisTokenStorage{redis: redis}
+}
+
+func NewRedisTokenStorageMock() (TokenStorage, error) {
+	mr, err := miniredis.Run()
+	if err != nil {
+		return nil, err
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	return &redisTokenStorage{
+		redis: rdb,
+	}, nil
 }
 
 func (s *redisTokenStorage) GetTokenData(token string) (string, error) {
