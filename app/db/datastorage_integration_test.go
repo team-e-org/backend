c45d40549344f418bfe7bab604a306a6d4bddb4c
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-23 17:47:16 +0900

    modify config read

--- app/db/datastorage_integration_test.go
+++ app/db/datastorage_integration_test.go
@@ -13,11 +13,11 @@ import (
 var sqlDB *sql.DB
 
 func dbHandlingWrapper(m *testing.M) int {
-	c, err := config.ReadDBConfig()
+	c, err := config.ReadConfig()
 	if err != nil {
 		panic(err)
 	}
-	sqlDB, err := ConnectToMySql(c.Host, c.Port, c.User, c.Password, c.DBName, c.TimeZone)
+	sqlDB, err := ConnectToMySql(c.DB.Host, c.DB.Port, c.DB.User, c.DB.Password, c.DB.DBName, c.DB.TimeZone)
 	if err != nil {
 		log.Panicf("Can not connect to DB: %v", err)
 	}
