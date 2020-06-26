5be82aecf8aa686550d4d5bdb9ea2ef94a8939cd
coil398 <konayuta.sou@gmail.com>
2020-06-26 16:57:47 +0900

    add update user

--- app/server/handlers/user.go
+++ app/server/handlers/user.go
@@ -67,7 +67,7 @@ func ServeUser(data db.DataStorageInterface, authLayer authz.AuthLayerInterface)
 
 		user, err := data.Users().GetUser(userID)
 		if err != nil {
-			logs.Error("Request: %s, gettign user from db: %v", requestSummary(r), err)
+			logs.Error("Request: %s, getting user from db: %v", requestSummary(r), err)
 			InternalServerError(w, r)
 			return
 		}
@@ -92,6 +92,70 @@ func ServeUser(data db.DataStorageInterface, authLayer authz.AuthLayerInterface)
 		}
 
 		w.Header().Set(contentType, jsonContent)
+		if _, err = w.Write(bytes); err != nil {
+			logs.Error("Request: %s, writing response: %v", requestSummary(r), err)
+		}
+	}
+}
+
+func UpdateUser(data db.DataStorageInterface, authLayer authz.AuthLayerInterface) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+
+		vars := mux.Vars(r)
+		userID, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			logs.Error("Request: %s, parse path parameter id: %v", requestSummary(r), err)
+			err := helpers.NewBadRequest(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		currentUserID, err := getUserIDIfAvailable(r, authLayer)
+		if err != nil {
+			logs.Error("Request: %s, checking if user identifiable: %v", requestSummary(r), err)
+			err := helpers.NewUnauthorized(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		if userID != currentUserID {
+			logs.Error("Request: %s, forbidden: %v", requestSummary(r), err)
+			err := helpers.NewUnauthorized(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		requestUser := &view.User{}
+		if err := json.NewDecoder(r.Body).Decode(requestUser); err != nil {
+			logs.Error("Request: %s, unable to parse content: %v", requestSummary(r), err)
+			err := helpers.NewBadRequest(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		user, err := view.NewUserModel(requestUser)
+		if err != nil {
+			logs.Error("Request: %s, an error occurred: %v", requestSummary(r), err)
+		}
+
+		userModel, err := usecase.UpdateUser(data, user, userID)
+		if err != nil {
+			logs.Error("Request: %s, %v", requestSummary(r), err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		bytes, err := json.Marshal(view.NewUser(userModel))
+		if err != nil {
+			logs.Error("Request: %s, serializing user: %v", requestSummary(r), err)
+			err := helpers.NewInternalServerError(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		w.Header().Set(contentType, jsonContent)
+		w.WriteHeader(http.StatusOK)
 		if _, err = w.Write(bytes); err != nil {
 			logs.Error("Request: %s, writing response: %v", requestSummary(r), err)
 		}
--- app/usecase/user.go
+++ app/usecase/user.go
@@ -6,6 +6,7 @@ import (
 	"app/helpers"
 	"app/logs"
 	"app/models"
+	"fmt"
 )
 
 func UserBoards(data db.DataStorageInterface, authLayer authz.AuthLayerInterface, userID int, currentUserID int) ([]*models.Board, helpers.AppError) {
@@ -24,6 +25,41 @@ func UserBoards(data db.DataStorageInterface, authLayer authz.AuthLayerInterface
 	}
 
 	return boards, nil
+}
+
+func UpdateUser(data db.DataStorageInterface, user *models.User, userID int) (*models.User, error) {
+	fmt.Printf("user: %v\n", *user)
+	fmt.Printf("userID: %d\n", userID)
+	u, err := data.Users().GetUser(userID)
+	if err != nil {
+		logs.Error("An error occurred while getting user data: %v", err)
+		return nil, helpers.NewInternalServerError(err)
+	}
+
+	if user.ID != u.ID {
+		logs.Error("UserIDs do not match error: %v", err)
+		return nil, helpers.NewUnauthorized(err)
+	}
+
+	if user.Name != "" {
+		u.Name = user.Name
+	}
+	if user.Email != "" {
+		u.Email = user.Email
+	}
+	if user.HashedPassword != "" {
+		u.HashedPassword = user.HashedPassword
+	}
+	if user.Icon != "" {
+		u.Icon = user.Icon
+	}
+
+	if err := data.Users().UpdateUser(u); err != nil {
+		logs.Error("An error occurred: %v", err)
+		return nil, helpers.NewInternalServerError(err)
+	}
+
+	return u, nil
 }
 
 func removePrivateBoards(boards []*models.Board, userID int) []*models.Board {
