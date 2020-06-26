b28942474eed266a63ba81f5d91713e9f39f5032
coil398 <konayuta.sou@gmail.com>
2020-06-26 18:33:17 +0900

    add update board

--- app/server/handlers/board.go
+++ app/server/handlers/board.go
@@ -8,7 +8,11 @@ import (
 	"app/usecase"
 	"app/view"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateBoard(data db.DataStorageInterface, authLayer authz.AuthLayerInterface) func(http.ResponseWriter, *http.Request) {
@@ -32,6 +36,73 @@ func CreateBoard(data db.DataStorageInterface, authLayer authz.AuthLayerInterfac
 		}
 
 		storedBoard, err := usecase.CreateBoard(data, requestBoard, userID)
+		if err != nil {
+			logs.Error("Request: %s, %v", requestSummary(r), err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		bytes, err := json.Marshal(view.NewBoard(storedBoard))
+		if err != nil {
+			logs.Error("Request: %s, serializing board: %v", requestSummary(r), err)
+			err := helpers.NewInternalServerError(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		w.Header().Set(contentType, jsonContent)
+		w.WriteHeader(http.StatusCreated)
+		if _, err = w.Write(bytes); err != nil {
+			logs.Error("Request: %s, writing response: %v", requestSummary(r), err)
+		}
+	}
+}
+
+func UpdateBoard(data db.DataStorageInterface, authLayer authz.AuthLayerInterface) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+		vars := mux.Vars(r)
+		boardID, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			logs.Error("Request: %s, parse path parameter id: %v", requestSummary(r), err)
+			err := helpers.NewBadRequest(err)
+			ResponseError(w, r, err)
+		}
+
+		userID, err := getUserIDIfAvailable(r, authLayer)
+		if err != nil {
+			logs.Error("Request: %s, checking if user identifiable: %v", requestSummary(r), err)
+			err := helpers.NewUnauthorized(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		requestBoard := &view.Board{}
+		if err := json.NewDecoder(r.Body).Decode(requestBoard); err != nil {
+			logs.Error("Request: %s, unable to parse content: %v", requestSummary(r), err)
+			err := helpers.NewBadRequest(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		if userID != requestBoard.UserID {
+			logs.Error("Request: %s, forbidden: %v", requestSummary(r), err)
+			err := helpers.NewUnauthorized(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		if boardID != requestBoard.ID {
+			err := fmt.Errorf("BoardIDs do not match error")
+			logs.Error("Request: %s, an error occurred: %v", requestSummary(r), err)
+			err = helpers.NewBadRequest(err)
+			ResponseError(w, r, err)
+			return
+		}
+
+		newBoard := view.NewBoardModel(requestBoard)
+
+		storedBoard, err := usecase.UpdateBoard(data, newBoard)
 		if err != nil {
 			logs.Error("Request: %s, %v", requestSummary(r), err)
 			ResponseError(w, r, err)
