c46cf5c97818ed0f8a1191e148ce76387733d00d
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-26 11:15:27 +0900

    replace init data

--- db/mysql/migrations/0000010_init_pins.sql
+++ db/mysql/migrations/0000010_init_pins.sql
@@ -6,96 +6,96 @@ VALUES
     1,
     "pin1",
     "pin1 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/009174cf-c4fe-4a49-a4b2-cb24a98887c9.png"
   ),
   (
     2,
     1,
     "pin2",
     "pin2 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/02b4ba8f-4917-41b7-93ba-357ae5eedb21.png"
   ),
   (
     3,
     1,
     "pin3",
     "pin3 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/7c11f134-7ce2-4617-8222-975996e79702.png"
   ),
   (
     4,
     1,
     "pin4",
     "pin4 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/585e8b79-5c5b-40d4-aaed-c7cc379191cc.png"
   ),
   (
     5,
     1,
     "pin5",
     "pin5 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/6a977236-0765-4de1-96cb-57ba6d449a1e.png"
   ),
   (
     6,
     1,
     "pin6",
     "pin6 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/d5d42ce0-5ac3-45fd-b462-3be5e65e6972.png"
   ),
   (
     7,
     1,
     "pin7",
     "pin7 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/859a604a-72e5-45fa-9e12-b4ffbd9e2f33.png"
   ),
   (
     8,
     1,
     "pin8",
     "pin8 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/e5126b56-e0e8-4fab-b6c7-ae8cf5cc02c4.png"
   ),
   (
     9,
     1,
     "pin9",
     "pin9 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/c446ea59-756f-4c13-a26e-04d92a4c963e.png"
   ),
   (
     10,
     1,
     "pin10",
     "pin10 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/79f8c86f-4aa5-4d2f-8d79-283436a71036.png"
   ),
   (
     11,
     1,
     "pin11",
     "pin11 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/17428619-0531-4831-af0d-1e296f3a7a75.png"
   ),
   (
     12,
     1,
     "pin12",
     "pin12 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/5e50a994-1910-4192-a4f7-f3acf6931924.png"
   ),
   (
     13,
     1,
     "pin13",
     "pin13 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/3ee3cc21-229a-4f4c-a1c8-cce359e68807.png"
   ),
   (
     14,
     1,
     "pin14",
     "pin14 description",
-    "https://2.bp.blogspot.com/-9wlENBcCWIM/XMZ9zUJZMOI/AAAAAAABSlY/pfuusOBVEz4LVyy3-d21JS1MuzvInErbgCLcBGAs/s800/drink_tapioka_tea_schoolboy.png"
+    "https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/e8d5da44-6f10-4278-b639-77580b135a4e.png"
   );
