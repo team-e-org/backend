5d281484b0e855809a95c43f94e5e092c68db294
Naoto Ishida <naoto.ishida@dena.com>
2020-07-01 14:18:10 +0900

    Implement delete board pin sql

--- app/infrastructure/board_pin.go
+++ app/infrastructure/board_pin.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"app/helpers"
 	"app/logs"
 	"app/repository"
 	"database/sql"
@@ -35,6 +36,27 @@ VALUES
 		logs.Error("An error occurred: %v", err)
 		return err
 	}
+
+	return nil
+}
+
+func (bp *BoardPin) DeleteBoardPin(boardID int, pinID int) error {
+	const query = `
+DELETE FROM boards_pins
+WHERE board_id = ? and pin_id = ?;
+`
+
+	stmt, err := bp.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(boardID, pinID)
+	if err = helpers.CheckDBExecError(result, err); err != nil {
+		return err
+	}
+
+	logs.Info("boards pins deleted, boardID: %d, pinID: %d", boardID, pinID)
 
 	return nil
 }
--- app/infrastructure/board_pin_test.go
+++ app/infrastructure/board_pin_test.go
@@ -49,3 +49,24 @@ func TestCreateBoardPinError(t *testing.T) {
 		t.Fatalf("Unfulfilled expectations error: %v\n", err)
 	}
 }
+
+func TestBoardPin_DeleteBoardPin(t *testing.T) {
+	boardID := 0
+	pinID := 0
+
+	query := "DELETE FROM boards_pins WHERE board_id = ? and pin_id = ?"
+	prepare := mock.ExpectPrepare(regexp.QuoteMeta(query))
+
+	prepare.ExpectExec().
+		WithArgs(boardID, pinID).
+		WillReturnResult(sqlmock.NewResult(int64(0), 1))
+	boardsPins := NewBoardPinRepository(sqlDB)
+	err := boardsPins.DeleteBoardPin(boardID, pinID)
+	if err != nil {
+		t.Fatalf("An error occurred: %v", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatalf("Unfulfilled expectations error: %v", err)
+	}
+}
